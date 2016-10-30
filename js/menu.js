78e81c8b908ab31535919a2396197e7b4871c13a
francojfernandez <francofernandez_0412@hotmail.com>
2016-10-29 23:31:13 -0300

    updates

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,6 @@
 var btnMenu = document.getElementById('btn-menu');
 var nav = document.getElementById('nav');
 
-//btnMenu
+btnMenu.addEventListener('click',function () {
+	nav.classList.toggle('mostrar');
+})
